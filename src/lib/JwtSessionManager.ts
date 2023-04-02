f984b7480e301d468201535e92a425af1736587c
Vincent Dubois <vincent.dubois@efrei.net>
2023-04-02 17:38:42 +0200

    Handle onNewActivity detected

--- src/lib/JwtSessionManager.ts
+++ src/lib/JwtSessionManager.ts
@@ -3,9 +3,11 @@ import { HttpError, HttpPromise } from 'simple-http-rest-client';
 import { Job, Scheduler } from 'simple-job-scheduler';
 import { Logger } from 'simple-logging-system';
 import { IdlenessDetector } from './IdlenessDetector';
-import { PageActivityManager, PageActivity } from './page-activity/PageActivityManager';
+import { PageActivityManager } from './page-activity/PageActivityManager';
 
 const logger = new Logger('JwtSessionManager');
+
+const IDLENESS_CHECK_INTERVAL = 1000;
 
 export type ExpirableJwtValue = {
   exp: number;
@@ -211,38 +213,37 @@ export class JwtSessionManager<U extends ExpirableJwtValue> {
   }
 
   private startSessionRefreshAndIdleDetection(refreshDurationInMillis: number, inactiveDurationInMillis: number): void {
-    this.refreshSessionTokenScheduledJob = this.scheduler.schedule(
-      'Refresh session token',
-      () => this.refreshSession(),
-      refreshDurationInMillis,
-    );
+    this.startSessionRefresh(refreshDurationInMillis);
     this.idlenessDetector.startService(
       () => {
         logger.info('Idleness detected, disconnecting...');
-        this.discardSession();
+        this.refreshSessionTokenScheduledJob?.cancel();
       },
+      () => this.onNewUserActivityDetected(refreshDurationInMillis),
       inactiveDurationInMillis,
-      1000,
+      IDLENESS_CHECK_INTERVAL,
     );
-    this
-      .pageActivityManager
-      .startService((eventType: PageActivity) => this.onBrowserPageActivityChange(eventType));
   }
 
-  private onBrowserPageActivityChange(eventType: PageActivity): void {
-    if (eventType === PageActivity.ACTIVE) {
-      if (!this.isUserSessionValid(this.currentUserExpirationDateInSeconds)) {
-        logger.info('Expired session detected on browser page active, disconnecting...');
-        this.discardSession();
-      } else {
-        logger.info('Page became active, refresh token started...');
-        this.refreshSessionTokenScheduledJob?.execute();
-      }
+  private startSessionRefresh(refreshDurationInMillis: number): void {
+    this.refreshSessionTokenScheduledJob = this.scheduler.schedule(
+      'Refresh session token',
+      () => {
+        this.refreshSession()
+      },
+      refreshDurationInMillis,
+    );
+  }
+
+  private onNewUserActivityDetected(refreshDurationInMillis: number): boolean {
+    if (!this.isUserSessionValid(this.currentUserExpirationDateInSeconds)) {
+      logger.info('Expired session detected on browser page active, disconnecting...');
+      this.discardSession();
+      return true;
     }
-    if (eventType === PageActivity.INACTIVE) {
-      logger.info('Page became inactive, refresh token stopped...');
-      this.refreshSessionTokenScheduledJob?.cancel();
-    }
+    logger.info('Page became active, refresh token started...');
+    this.startSessionRefresh(refreshDurationInMillis);
+    return false; // idleness job must be restarted
   }
 
   private isUserSessionValid(expirationDateInSeconds?: number): boolean {
